baa89840e90d63f6ab031216daf4cd1e2e69d19e
Yash Vishwanath <yash.vishwanath@codewizards.co.uk>
2026-04-30 17:40:49 +0100

    Hitbox: Implement BoxData and BoxProfile
    BoxData is basic info about a Collider (size, offset and enabled)
    BoxFrameData stores the data of all a Fighter's boxes in 1 Frame
    BoxProfile is BoxFrameData in ScriptableObject form

---
+++ Assets/Scripts/Data/BoxData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [System.Serializable]
+    public struct BoxData
+    {
+        public bool Enabled;
+        public Vector2 Offset;
+        public Vector2 Size;
+    }
+    
+    [System.Serializable]
+    public class BoxFrameData
+    {
+        public BoxData Pushbox;
+        public BoxData[] Hurtboxes;
+        public BoxData[] Hitboxes;
+    }
+}
---
+++ Assets/Scripts/Data/BoxProfile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(menuName = "Fight Test/Combat Boxes/Box Profile")]
+    public class BoxProfile : ScriptableObject
+    {
+        public BoxFrameData Frame;
+    }
+}
