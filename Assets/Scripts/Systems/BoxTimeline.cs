d5d20f64eb8f934f52395f2dc2029e2a4956a6df
Yash Vishwanath <yash.vishwanath@codewizards.co.uk>
2026-05-01 01:29:37 +0100

    HitBox: BoxTimeline
    Timeline of Start and End Frames and their box statees

---
+++ Assets/Scripts/Systems/BoxTimeline.cs
@@ -0,0 +1,25 @@
+﻿using Data;
+using UnityEngine;
+
+namespace FightTest.Systems
+{
+    [CreateAssetMenu(menuName = "Fight Test/Combat Boxes/Box Timeline")]
+    public class BoxTimeline : ScriptableObject
+    {
+        public BoxTimelineData[] Entries;
+
+        public BoxFrameData GetFrame(int frame)
+        {
+            if (Entries == null)
+                return null;
+
+            foreach (var entry in Entries)
+            {
+                if (entry.Contains(frame))
+                    return entry.Frame;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/BoxTimelineData.cs
@@ -0,0 +1,17 @@
+﻿using Data;
+
+namespace FightTest.Systems
+{
+    [System.Serializable]
+    public class BoxTimelineData
+    {
+        public int StartFrame;
+        public int EndFrame;
+        public BoxFrameData Frame;
+
+        public bool Contains(int frame)
+        {
+            return frame >= StartFrame && frame <= EndFrame;
+        }
+    }
+}
