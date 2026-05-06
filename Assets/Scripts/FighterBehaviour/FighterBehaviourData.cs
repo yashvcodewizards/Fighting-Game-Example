cb899eb17ce2f329780aa5678812e5b3510c45e8
Yash Vishwanath <yash.vishwanath@codewizards.co.uk>
2026-05-06 16:51:56 +0100

    Refactor: FighterBehaviourData

---
+++ Assets/Scripts/FighterBehaviour/FighterBehaviourData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace FighterBehaviour
+{
+    public abstract class FighterBehaviourData : ScriptableObject
+    {
+        public abstract FighterStateRegistry BuildStates(FighterRuntime runtime);
+
+        public abstract void Initialize(FighterRuntime runtime);
+    }
+}
